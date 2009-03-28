665fc178a73e7aa4ebcfaf2d023de6c4d46f09f0
gmcgarry <gmcgarry>
2009-03-28 07:04:06 +0000

    fistp -> fistpl

--- libpcc/_ftol.c
+++ libpcc/_ftol.c
@@ -1,4 +1,4 @@
-/*	$Id: _ftol.c,v 1.1 2009/03/15 00:17:16 gmcgarry Exp $	*/
+/*	$Id: _ftol.c,v 1.2 2009/03/28 07:04:06 gmcgarry Exp $	*/
 
 /*
  * _ftol() and _ftol2() implementations used on win32 when linking against
@@ -15,7 +15,7 @@ asm(	"	.text\n"
 	"	or %ax,0x0c00\n"
 	"	movw %ax,-4(%esp)\n"
 	"	fldcw -4(%esp)\n"
-	"	fistp -12(%esp)\n"
+	"	fistpl -12(%esp)\n"
 	"	fldcw -2(%esp)\n"
 	"	movl -12(%esp),%eax\n"
 	"	movl -8(%esp),%edx\n"
